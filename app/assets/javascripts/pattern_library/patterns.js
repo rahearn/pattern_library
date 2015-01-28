4e5b5ed3a75113cd5baecde99dbf11e30298cce5
Ryan Ahearn <ryan.c.ahearn@gmail.com>
2015-01-27 21:14:05 -0500

    Pretty print relevant css rules

--- app/assets/javascripts/pattern_library/patterns.js
+++ app/assets/javascripts/pattern_library/patterns.js
@@ -5,43 +5,37 @@ $(function() {
   $('#pl-patterns .pl-pattern').each(function() {
     var $this = $(this);
     $this.find('.pl-snippet-styled > *').each(function() {
-      $this.find('.pl-pattern-css').text(prettyPrintJSON(css($(this))));
+      var styles = css($(this));
+      $(this).find('*').each(function() {
+        styles = styles.concat(css($(this)));
+      });
+      $this.find('.pl-pattern-css').text(prettyPrintArray(styles));
     });
   });
 });
 
 function css(a) {
-  var sheets = document.styleSheets, o = {};
+  var sheets = document.styleSheets, o = [];
   for (var i in sheets) {
     var rules = sheets[i].rules || sheets[i].cssRules;
     for (var r in rules) {
       if (a.is(rules[r].selectorText)) {
-        o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
+        o.push(rules[r].cssText);
       }
     }
+  }
+  if (a.attr('style')) {
+    o.push("Inline { " + a.attr('style') + " }");
   }
   return o;
 }
 
-function css2json(css) {
-  var s = {};
-  if (!css) return s;
-  if (css instanceof CSSStyleDeclaration) {
-    for (var i in css) {
-      if ((css[i]).toLowerCase) {
-        s[(css[i]).toLowerCase()] = (css[css[i]]);
-      }
-    }
-  } else if (typeof css == "string") {
-    css = css.split(/;\s*/);
-    for (var i in css) {
-      var l = css[i].split(/:\s*/);
-      s[l[0].toLowerCase()] = (l[1]);
-    }
-  }
-  return s;
-}
-
-function prettyPrintJSON(json) {
-  return JSON.stringify(json);
+function prettyPrintArray(styles) {
+  return $.map(styles, function(rule) {
+    rule = rule.replace(/\{\s*/g, '{\n  ');
+    rule = rule.replace(/;\s*/g, ';\n  ');
+    rule = rule.replace(/([^;\s])\s*\}/g, '$1;\n}');
+    rule = rule.replace(/^\s+\}/gm, '}');
+    return rule;
+  }).join('\n');
 }
