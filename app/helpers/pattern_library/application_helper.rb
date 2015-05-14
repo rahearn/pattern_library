a64ee4da57620c603e76aa93f28f36dd054e6389
Ryan Ahearn <ryan.c.ahearn@gmail.com>
2015-05-14 10:26:39 -0400

    Break engine isolation a bit.
    Allow application route helpers to be accessed if they dont match
    anything in the engine first. Needed to embed pages in app layout

--- app/helpers/pattern_library/application_helper.rb
+++ app/helpers/pattern_library/application_helper.rb
@@ -1,4 +1,12 @@
 module PatternLibrary
   module ApplicationHelper
+    def method_missing(method, *args, &block)
+      method_name = method.to_s
+      if (method_name.end_with?('_path') || method_name.end_with?('_url')) && main_app.respond_to?(method)
+        main_app.send method, *args
+      else
+        super
+      end
+    end
   end
 end
